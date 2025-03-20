c139f079985afdc722578c5761097995a1a829da
斯卡曼德骨科诊所 <109299529+Volga-Vodka@users.noreply.github.com>
2025-03-20 09:07:56 +0800

    ip_sb.js

--- sample-location-with-script.js
+++ sample-location-with-script.js
@@ -9,9 +9,9 @@ var obj = JSON.parse(body);
 var title = obj['country'] || '未知国家';
 var subtitle = (obj['city'] || '未知城市') + ' ' + (obj['isp'] || '未知运营商');
 var ip = obj['ip'] || '未知IP';
-var description = "国家: " + title + '\n' +
-                      "城市: " + (obj['city'] || '未知') + '\n' +
-                      "运营商: " + (obj['isp'] || '未知') + '\n' +
-                      "数据中心: " + (obj['organization'] || '未知') + '\n' +
-                      "ASN: " + (obj['asn'] || '未知') + '\n' +
-    $done({title, subtitle, ip, description});
+var description = "国家: " + title + '\n' + "城市: " + 
+(obj['city'] || '未知') + '\n' + "运营商: " + 
+(obj['isp'] || '未知') + '\n' + "数据中心: " + 
+(obj['organization'] || '未知') + '\n' + "ASN: " + 
+(obj['asn'] || '未知');
+$done({title, subtitle, ip, description});
